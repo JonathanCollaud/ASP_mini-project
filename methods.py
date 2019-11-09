0a98c5562987be191edfeb9e979f3631ebf8b5c6
Damien Ronssin <damien.ronssin@epfl.ch>
2019-11-09 15:39:00 +0100

    Working pitch shifting, without windowing

---
+++ methods.py
@@ -0,0 +1,125 @@
+import pyaudio
+import wave
+import numpy as np
+from scipy import interpolate as interp
+
+
+def spline(x, y, x_new):
+    tck_r = interp.splrep(x, np.real(y), s=0)
+    tck_i = interp.splrep(x, np.imag(y), s=0)
+    y_new_r = interp.splev(x_new, tck_r, der=0)
+    y_new_i = interp.splev(x_new, tck_i, der=0)
+    y_new = y_new_r + 1j * y_new_i
+    return y_new
+
+
+def spline_polar(x, y, x_new):
+    amp, phase = C2P(y)
+
+    tck_r = interp.splrep(x, amp, s=0)
+    tck_i = interp.splrep(x, phase, s=0)
+
+    y_new_amp = interp.splev(x_new, tck_r, der=0)
+    y_new_phase= interp.splev(x_new, tck_i, der=0)
+
+    y_new = P2C(y_new_amp, y_new_phase)
+    return y_new
+
+
+def interp1d(x, y, x_new):
+    f_r = interp.interp1d(x, np.real(y), bounds_error=False, fill_value=0.0)
+    f_i = interp.interp1d(x, np.imag(y), bounds_error=False, fill_value=0.0)
+    out = f_r(x_new) + 1j * f_i(x_new)
+    return out
+
+
+def interp1d_polar(x, y, x_new):
+    amp, phase = C2P(y)
+    f_amp = interp.interp1d(x, amp, bounds_error=False, fill_value=0.0)
+    f_phase = interp.interp1d(x, phase, bounds_error=False, fill_value=0.0)
+    out = P2C(f_amp(x_new), f_phase(x_new))
+    return out
+
+
+def shift_factor(y, freq, notes, notes_name):
+    """ Compute the shift factor to apply to frequencies.
+    :param y: Fourier transform (get via np.rfft)
+    :param freq: Frequency corresponding to each index (get via np.rfftfreq)
+    :param notes: notes table containing all notes (defined at beginning of code
+    :return: shift factor
+    """
+
+    idx_max = np.argmax(y*np.conj(y))
+
+    # if measured freq is 0, be careful, do not divide by 0
+    if idx_max != 0:
+        pitch = freq[idx_max]
+    else:
+        pitch = freq[1]
+
+    closest_note_idx = np.argmin(np.abs(pitch-notes))
+    closest_note = notes[closest_note_idx]
+    print("Closest note: ", notes_name[closest_note_idx], end='\r')
+
+    # Computation of shift factor: coeff to apply to frequency of input signal
+    shift_f = closest_note / pitch
+    return shift_f
+
+
+def processing(data, freq, notes, chunk_size, pad_size, notes_name):
+    x = np.frombuffer(data, dtype=np.int16)
+    # Zero Padding:
+    x = np.pad(x, (0, pad_size), 'constant', constant_values=(0, 0))
+    x = x.astype(np.int16)
+
+    # Real fft
+    y = np.fft.rfft(x)
+
+    # Compute shift factor
+    shift_f = shift_factor(y, freq, notes, notes_name)
+    #print(shift_f)
+
+    # shift_f = 4/5
+    # Shift frequency spectrum
+    y_new = shift_freq(y, freq, shift_f)
+
+    # y_new = y
+    # Inverse FFT and take real part
+    out = np.fft.irfft(y_new)
+
+    # Remove zero padding
+    out = out[:chunk_size]
+
+    out = np.real(out)
+    return out
+
+
+def shift_freq(y, freq, shift_f):
+    # Interpolation: suppose you have correct pitch (freq_x = shift_f * freq) and resample to freq scale
+    # to then do inverse fourier transform
+    y_new = interp1d_polar(shift_f * freq, y, freq)
+    # y_new = y
+    return y_new
+
+
+def play(stream, chunk):
+    stream.write(chunk)
+
+
+def P2C(r, angles):
+    """
+    Take a complex number in polar form and put it in cartesian
+    :param r: radius
+    :param angles: phase in radian
+    :return: complex number in cartesian coordinates
+    """
+    return r * np.exp(1j*angles)
+
+
+def C2P(x):
+    """
+    Take a complex np array and return a tuple of np array with norm and phase (polar coordinates)
+    :param x: complex np array
+    :return: 2D array with norm and phase
+    """
+    return np.abs(x), np.angle(x)
--- test.py
+++ test.py
@@ -1,52 +1,72 @@
 import pyaudio
 import wave
-import sys
 import numpy as np
+from methods import *
 
-CHUNK = int(512)
+np.set_printoptions(4, suppress=True)
+
+
+CHUNK_SIZE = int(512)
+
+# 0 : no padding, 1: half signal half zeros ...
+FFT_SIZE = 2**0 * CHUNK_SIZE
+
 FORMAT = pyaudio.paInt16
-CHANNELS = 2
+CHANNELS = 1
 RATE = 44100
-RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "voice.wav"
+RECORD_SECONDS = 4
+WAVE_OUTPUT_FILENAME = "voice_modif.wav"
+WAVE_OUTPUT_FILENAME_NO_MODIF = "voice_no_modif.wav"
 
 
-def processing(data):
-    x = np.frombuffer(data, dtype=np.int16)
-    x = np.fft.fft(x)
-    z = shift_freq(x, 2)
-    out = np.fft.ifft(z)
-    out = np.real(out)
-    return out
+# Size of padding at the end
+pad_size = int(FFT_SIZE - CHUNK_SIZE)
 
-## test
-def shift_freq(f_t_vect, corr_f):
-    idx = np.arange(f_t_vect.shape[0])
-    z = np.zeros_like(f_t_vect)
-    reag = np.rint(idx/corr_f)
-    reag = reag.astype(np.int16)
-    z = z + f_t_vect[reag]
-    return z
+print('Padding:', pad_size)
+# number of octave
+n_oct = 5
+
+# Name of notes
+notes_str = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+
+# If we accept only A minor (C major) notes
+notes_accepted = ['A', 'B', 'C', 'D', 'E', 'F', 'G']
+# gives this table
+n_accepted_notes = np.array([0, 2, 3, 5, 7, 8, 10])
+n_extended = np.array([])
+
+for i in range(n_oct+1):
+    n_extended = np.concatenate((n_extended, 12.0 * i + n_accepted_notes), axis=0)
+
+notes_name = []
+for i in range(n_oct+1):
+    notes_name = notes_name + [n + str(i+1) for n in notes_accepted]
 
 
+# Notes for our table of notes
+notes = np.asarray(55.0*2.0**(n_extended/12.0))
 
-
-def play(stream, chunk):
-    stream.write(chunk)
+# Frequencies of real fft
+freq = np.fft.rfftfreq(FFT_SIZE, 1.0/RATE)
 
 p = pyaudio.PyAudio()
 
-in_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK)
+in_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True, frames_per_buffer=CHUNK_SIZE)
 
-out_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK)
+out_stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, output=True, frames_per_buffer=CHUNK_SIZE)
 
 print("* recording")
 
 frames = []
+frames_no_modif = []
 
-for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
-    chunk = in_stream.read(CHUNK)
-    processed_chunk = processing(chunk)
+
+for i in range(0, int(RATE / CHUNK_SIZE * RECORD_SECONDS)):
+    chunk = in_stream.read(CHUNK_SIZE)
+    frames_no_modif.append(chunk)
+
+    processed_chunk = processing(chunk, freq, notes, CHUNK_SIZE, pad_size, notes_name)
+
     processed_chunk = processed_chunk.astype(np.int16).tostring()
     play(out_stream, processed_chunk)
     frames.append(processed_chunk)
@@ -64,4 +84,11 @@ wf.setnchannels(CHANNELS)
 wf.setsampwidth(p.get_sample_size(FORMAT))
 wf.setframerate(RATE)
 wf.writeframes(b''.join(frames))
+wf.close()
+
+wf = wave.open(WAVE_OUTPUT_FILENAME_NO_MODIF, 'wb')
+wf.setnchannels(CHANNELS)
+wf.setsampwidth(p.get_sample_size(FORMAT))
+wf.setframerate(RATE)
+wf.writeframes(b''.join(frames_no_modif))
 wf.close()
